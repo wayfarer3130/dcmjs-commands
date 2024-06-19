a312af463b411986a590a2c6541f2f6f558c1c97
Bill Wallace <wayfarer3130@gmail.com>
2024-06-19 10:24:40 -0400

    fix: Add dump command and instance command

--- bin/dcmjs.js
+++ bin/dcmjs.js
@@ -1,30 +1,31 @@
 #!/usr/bin/env node
-const { Command } = require('commander');
-const dcmjs = require('dcmjs');
-const fs = require('fs');
-
-const { DicomMessage } = dcmjs.data;
+import { Command } from 'commander';
+import { readDicom, instanceDicom, dumpDicom } from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('dcmjs')
   .description('dcmjs based tools for manipulation DICOM files')
-  .version('0.0.1');
+  .version('0.0.1')
 
 program.command('dump')
   .description('Dump a dicom file')
-  .argument('<part10>', 'part 10 file to dump')
+  .argument('<part10>', 'part 10 file')
   // .option('-s, --separator <char>', 'separator character', ',')
-  .action((fileName, _options) => {
-    console.log("Dumping file 2", fileName);
-    const arrayBuffer = fs.readFileSync(fileName).buffer;
-    console.log("Read file of length", arrayBuffer.length);
-    const dicomDict = DicomMessage.readFile(arrayBuffer);
-    console.log("Metadata");
-    console.log("", JSON.stringify(dicomDict.meta, null, 2));
-    console.log("Data");
-    console.log('', JSON.stringify(dicomDict.dict, null, 2));
+  .action(async (fileName, _options) => {
+    const dicomDict = readDicom(fileName);
+    dumpDicom(dicomDict);
   });
+
+program.command('instance')
+  .description('Write the instance data')
+  .argument('<part10>', 'part 10 file')
+  .option('-p, --pretty', 'Pretty print')
+  .action(async (fileName, options) => {
+    const dicomDict = readDicom(fileName);
+    instanceDicom(dicomDict, options);
+  })
+
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,78 @@
+import fs from "fs";
+import dcmjs from "dcmjs";
+
+const { DicomMessage, DicomMetaDictionary } = dcmjs.data;
+
+export function readDicom(fileName) {
+  const arrayBuffer = fs.readFileSync(fileName).buffer;
+  const dicomDict = DicomMessage.readFile(arrayBuffer);
+  return dicomDict;
+}
+
+export function dumpDicom(dicomDict, options = {}) {
+  if (dicomDict.meta) {
+    console.log("Metadata");
+    dumpData(dicomDict.meta, options);
+  }
+  console.log("Data");
+  dumpData(dicomDict.dict, options);
+}
+
+export function dumpData(data, options, indent = '') {
+  if (typeof data !== 'object') {
+    return;
+  }
+  const keys = Object.keys(data).sort();
+  for (const key of keys) {
+    const value = data[key]
+    if (!value) {
+      continue;
+    }
+    const { vr } = value;
+    const punctuatedTag = DicomMetaDictionary.punctuateTag(key);
+    const entry = DicomMetaDictionary.dictionary[punctuatedTag];
+    const name = entry?.name || '';
+    if (vr === 'SQ') {
+      console.log(indent, key, name);
+      dumpSq(name || key, value, options, indent + '  ');
+      continue;
+    }
+    console.log(indent, key, name, valueToString(value, options));
+  }
+}
+
+export function valueToString(value, options) {
+  const { Value: values } = value;
+  if (!values) {
+    console.log("***** Value = ", value);
+    return '';
+  }
+  if (values.length === 0) return '';
+  const [v0] = values;
+  if (v0 instanceof ArrayBuffer) {
+    return `ArrayBuffer of length ${values.length}`;
+  }
+  if (typeof v0 === 'object') {
+    return values.map(it => JSON.stringify(it)).join(', ');
+  }
+  return values.map(it => String(it)).join(', ');
+}
+
+export function dumpSq(tag, value, options, indent) {
+  const { Value: sq } = value;
+  if (sq?.length === undefined) {
+    console.log("**** undefined value length", value);
+    return;
+  }
+  for (let i = 0; i < sq.length; i++) {
+    console.log(indent, "Item #", i + 1);
+    dumpData(sq[i], options, indent + '  ');
+  }
+  console.log(indent, 'End of', tag, "with", sq.length, 'items');
+}
+
+export function instanceDicom(dicomDict, options = {}) {
+  const { pretty } = options;
+  const result = pretty ? JSON.stringify(dicomDict.dict, null, 2) : JSON.stringify(dicomDict.dict);
+  console.log('', result);
+}
